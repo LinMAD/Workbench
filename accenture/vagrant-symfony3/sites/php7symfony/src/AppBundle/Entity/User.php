f70faf099df12cdd5c03902067d3d458066cf703
Artjoms Nemiro <nemtjom@hotmail.com>
2017-04-29 16:31:03 +0300

    Added new user entity

---
+++ accenture/vagrant-symfony3/sites/php7symfony/src/AppBundle/Entity/User.php
@@ -0,0 +1,22 @@
+<?php
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="users")
+ * @ORM\Entity()
+ *
+ * @package AppBundle\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
+     */
+    protected $id;
+}
