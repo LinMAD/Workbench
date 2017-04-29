6c8f49ff24fc760692ff6f67acf4fd4c3627b12f
Artjoms Nemiro <nemtjom@hotmail.com>
2017-04-29 17:32:07 +0300

    Added sec controllers and configs

---
+++ accenture/vagrant-symfony3/sites/php7symfony/src/AppBundle/Security/ApiAuthenticator.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AppBundle\Security;
+
+use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Authentication\Token\JWTUserToken;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\AuthorizationHeaderTokenExtractor;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorInterface;
+
+class ApiAuthenticator implements GuardAuthenticatorInterface
+{
+    /** @var \Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface  */
+    protected $encoder;
+
+    /** @var \Doctrine\ORM\EntityManager */
+    protected $em;
+
+    public function __construct(EntityManager $em, JWTEncoderInterface $encoder)
+    {
+        $this->em = $em;
+        $this->encoder = $encoder;
+    }
+
+    public function start(Request $request, AuthenticationException $authException = NULL)
+    {
+        return new Response('Auth header required', 401);
+    }
+
+    public function getCredentials(Request $request)
+    {
+        $extractor = new AuthorizationHeaderTokenExtractor(
+            'Bearer',
+            'Authorization'
+        );
+
+        $token = $extractor->extract($request);
+
+        if (false === $token) {
+            return;
+        }
+
+        return $token;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        try {
+            $data = $this->encoder->decode($credentials);
+        } catch (JWTDecodeFailureException $e) {
+            return null;
+        }
+
+        if (!$data) {
+            return null;
+        }
+
+        return $userProvider->loadUserByUsername($data['username']);;
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    public function createAuthenticatedToken(UserInterface $user, $providerKey)
+    {
+        return new JWTUserToken($user->getRoles(), $user);
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        return new JsonResponse(['message' => $exception->getMessageKey()], 401);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return;
+    }
+
+    public function supportsRememberMe()
+    {
+        return;
+    }
+
+}
