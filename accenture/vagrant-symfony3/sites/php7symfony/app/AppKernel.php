220e952a52af15f1dc2fb778d433be8bb95a9ed0
Artjoms Nemiro <nemtjom@hotmail.com>
2017-04-29 14:13:41 +0300

    Added lib to composer

--- accenture/vagrant-symfony3/sites/php7symfony/app/AppKernel.php
+++ accenture/vagrant-symfony3/sites/php7symfony/app/AppKernel.php
@@ -1,5 +1,6 @@
 <?php
 
+use FOS\RestBundle\FOSRestBundle;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -16,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FOSRestBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
