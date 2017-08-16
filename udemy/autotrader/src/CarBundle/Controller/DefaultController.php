2d7d59937aaaa2c5ca36e7c47b96bee154fcde21
Artjoms Nemiro <nemtjom@hotmail.com>
2017-08-16 00:32:29 -0500

    Added entity and database data

--- udemy/autotrader/src/CarBundle/Controller/DefaultController.php
+++ udemy/autotrader/src/CarBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace CarBundle\Controller;
 
+use CarBundle\Entity\Car;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -9,9 +10,35 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/our-cars", name="offer")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \LogicException
      */
     public function indexAction()
     {
-        return $this->render('CarBundle:Default:index.html.twig');
+        $carRepository = $this->getDoctrine()->getRepository(Car::class);
+        $cars = $carRepository->findAll();
+
+        return $this->render('CarBundle:Default:index.html.twig', [
+            'cars' => $cars,
+        ]);
+    }
+
+    /**
+     * @Route("/car/{id}", name="show_car")
+     *
+     * @param $id
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showAction($id): \Symfony\Component\HttpFoundation\Response
+    {
+        $carRepository = $this->getDoctrine()->getRepository(Car::class);
+        $car = $carRepository->find($id);
+
+        return $this->render('CarBundle:Default:show.html.twig', [
+            'car' => $car,
+        ]);
     }
 }
---
+++ udemy/autotrader/src/CarBundle/Entity/Car.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace CarBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Car
+ *
+ * @ORM\Table(name="car")
+ * @ORM\Entity(repositoryClass="CarBundle\Repository\CarRepository")
+ */
+class Car
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="make", type="string", length=255)
+     */
+    private $make;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="decimal", scale=2)
+     */
+    private $price;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="year", type="integer")
+     */
+    private $year;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="navigation", type="boolean")
+     */
+    private $navigation;
+
+    /**
+     * @var string
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Car
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set make
+     *
+     * @param string $make
+     *
+     * @return Car
+     */
+    public function setMake($make)
+    {
+        $this->make = $make;
+
+        return $this;
+    }
+
+    /**
+     * Get make
+     *
+     * @return string
+     */
+    public function getMake()
+    {
+        return $this->make;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param int $year
+     */
+    public function setYear(int $year)
+    {
+        $this->year = $year;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isNavigation(): ?bool
+    {
+        return $this->navigation;
+    }
+
+    /**
+     * @param bool $navigation
+     */
+    public function setNavigation(bool $navigation)
+    {
+        $this->navigation = $navigation;
+    }
+}
+
