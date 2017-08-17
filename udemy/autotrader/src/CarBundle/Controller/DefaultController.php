722dd470a8aac642b65320f0711393b79aea8dc4
Artjoms Nemiro <nemtjom@hotmail.com>
2017-08-16 20:30:45 -0500

    Added serch form

--- udemy/autotrader/src/CarBundle/Controller/DefaultController.php
+++ udemy/autotrader/src/CarBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace CarBundle\Controller;
 use CarBundle\Entity\Car;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
@@ -21,8 +24,14 @@ class DefaultController extends Controller
         $carRepository = $this->getDoctrine()->getRepository(Car::class);
         $cars = $carRepository->findCarsWithDetails();
 
+        $form = $this->createFormBuilder()
+            ->setMethod(Request::METHOD_GET)
+            ->add('search', TextType::class)
+            ->getForm();
+
         return $this->render('CarBundle:Default:index.html.twig', [
             'cars' => $cars,
+            'form' => $form->createView()
         ]);
     }
 
