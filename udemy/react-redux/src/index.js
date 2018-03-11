a153f75fc7c887962704808fee33a4eceebb5088
Artjoms Nemiro <nemtjom@hotmail.com>
2018-03-11 23:29:20 +0200

    Fetch video from youtube

--- udemy/react-redux/src/index.js
+++ udemy/react-redux/src/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {Component} from "react/lib/ReactIsomorphic";
+import YTSearch from "youtube-api-search";
 import SearchBar from "./Components/search_bar";
 
 // Key for Youtube v3 API, restricted by IP
-const Y_API_KEY = 'AIzaSyC2zRFqFVwOqJB1Vl2NNpYJ96NirRuL3X4';
+const YT_API_KEY = 'AIzaSyC2zRFqFVwOqJB1Vl2NNpYJ96NirRuL3X4';
 
-const App = () => {
-  return (
-      <div>
-          <SearchBar/>
-      </div>
-  );
-};
+// Main app component
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: YT_API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+}
 
 // Render component's HTML and put it to page
 ReactDOM.render(
